060e2308b589adb8e147852910e38c484ffb1404
sphanit <thedecipher.sp@gmail.com>
2022-07-20 01:42:31 +0200

    updated data collection GUI

--- invisible_humans_detection/tests/data_gui.py
+++ invisible_humans_detection/tests/data_gui.py
@@ -6,6 +6,7 @@
 import rospy
 from geometry_msgs.msg import PointStamped
 import PySimpleGUI as sg
+import yaml
 
 class GetPos(object):
   """docstring for GetPos."""
@@ -33,21 +34,28 @@ window = sg.Window('Get Data', layout)
 # Event Loop to process "events" and get the "values" of the inputs
 
 get_point = GetPos()
-
-time = 100
+data = dict()
+data['centers'] = []
+data['radii'] = []
 while True:
     if get_point.triggered:
-      window['-mytext-'].update('Got the Center')
-      print(get_point.center)
+      window['-mytext-'].update('Got the Center!')
+      data['centers'].append(get_point.center)
+      # print(get_point.center)
       event, values = window.read()
       if event == 'Add':
         window['-mytext-'].update('Added the data Point')
         event, values = window.read(1000)
+        data['radii'].append(float(values[0]))
       get_point.triggered = False;
     else:
       event, values = window.read(100)
-      window['-mytext-'].update('No center Found!')
+      window['-mytext-'].update('Waiting for the center...')
 
     if event == sg.WIN_CLOSED or event == 'Close': # if user closes window or clicks cancel
         break
+
+with open('test.yaml', 'w') as file:
+    documents = yaml.dump(data, file, default_flow_style=False)
+
 window.close()
